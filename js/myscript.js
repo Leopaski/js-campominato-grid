8d5e42b50f66f93496069b101ac7268791241124
Leopaski <leonardo.pascale@gmail.com>
2023-05-24 16:52:34 +0200

    clicked

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,22 @@
-const tabella = document.getElementById('tabella');
+let tabella = document.getElementById('tabella');
 function createCell(className){
-    const cell=document.createElement();
-    cell= cell.classList.add(className);
+    const cell=document.createElement('div');  
+   	cell.classList.add(className); 
     return cell;
 }
-for (i=0;i<100;i++){
-    tabella.appendChild(createCell(cella));
-}
+
+play.addEventListener('click', function(){
+    tabella.innerHTML="";
+    for (i=0;i<100;i++){
+        const newCell = createCell('div','cell ',
+                        `<p>${i + 1}</p>`);
+        newCell.addEventListener('click', function(){
+        this.classList.toggle('clicked');
+        tabella.appendChild(createCell("cella")); 
+    })}
+});
+
+
+
+
+
