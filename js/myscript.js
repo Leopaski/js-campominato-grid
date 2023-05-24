691117afe5e994a7879391e28c8fbf78bcf1e690
Leopaski <leonardo.pascale@gmail.com>
2023-05-24 17:25:30 +0200

    error

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,12 @@
-let tabella = document.getElementById('tabella');
+const tabella = document.getElementById('tabella');
 function createCell(className){
-    const cell=document.createElement('div');  
+    const cell=document.createElement('div'); 
    	cell.classList.add(className); 
     return cell;
 }
-
-play.addEventListener('click', function(){
-    tabella.innerHTML="";
-    for (i=0;i<100;i++){
-        const newCell = createCell('div','cell ',
-                        `<p>${i + 1}</p>`);
-        newCell.addEventListener('click', function(){
-        this.classList.toggle('clicked');
-        tabella.appendChild(createCell("cella")); 
-    })}
-});
+for (i=0;i<100;i++){
+    tabella.appendChild(createCell("cella")); 
+}
 
 
 
